2d039ee964c91cef5a6f95eff67803c51add6c60
krishnak-pandit <68848429+krishnak-pandit@users.noreply.github.com>
2025-11-26 11:39:05 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -30,10 +30,10 @@ function getDateAndTime(): string {
 
 export const DATE_AND_TIME = getDateAndTime();
 
-export const AI_NAME = "MyAI3";
-export const OWNER_NAME = "FirstName LastName";
+export const AI_NAME = "ElectiveAssistant";
+export const OWNER_NAME = "KK & Armaan";
 
-export const WELCOME_MESSAGE = `Hello! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
+export const WELCOME_MESSAGE = `Hello! I'm ${AI_NAME}, an AI assistant to help you choose the right courses`
 
 export const CLEAR_CHAT_TEXT = "New";
 
