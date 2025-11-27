ad0f78d42113844f7afaf6bd4156d238a126f975
krishnak-pandit <68848429+krishnak-pandit@users.noreply.github.com>
2025-11-27 17:13:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "ECHO",
+  description: "ECHO",
 };
 
 export default function RootLayout({
